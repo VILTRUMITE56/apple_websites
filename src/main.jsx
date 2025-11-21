8ce9c04414e5e0278174c339a67784c941c613e0
Gabriel Ybanez <ybagabrielfrancis@gmail.com>
2025-11-22 01:53:19 +0800

    sentry set up (for web optimization)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://b34d2cbf6fe917267d76948e119fd499@o4510397917429760.ingest.us.sentry.io/4510397919854592",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    // Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
